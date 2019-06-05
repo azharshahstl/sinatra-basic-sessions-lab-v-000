6e921777b36da74ed1da38481023a528d4f4f26d
Azhar Shah <azharshahstl@yahoo.com>
2019-06-05 21:36:51 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,8 +1,8 @@
 require_relative 'config/environment'
 
-onfigure do
-    enable :sessions
-    set :session_secret, "secret"
+  configure do
+      enable :sessions
+      set :session_secret, "secret"
   end
 
 class App < Sinatra::Base
